82609039a7957cd41f20fda1120b4c15ca11eef2
Dylan Beattoie <dylan@dylanbeattie.net>
2022-08-31 10:32:23 +0200

    Hypermedia actions with dynamic objects

---
+++ dotnet/Autobarn.Website/Controllers/HypermediaExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using Autobarn.Data.Entities;
+
+namespace Autobarn.Website.Controllers {
+    public static class HypermediaExtensions {
+        public static dynamic ToDynamic(this object value) {
+            IDictionary<string, object> expando = new ExpandoObject();
+            var properties = TypeDescriptor.GetProperties(value.GetType());
+            foreach (PropertyDescriptor property in properties) {
+                if (Ignore(property)) continue;
+                expando.Add(property.Name, property.GetValue(value));
+            }
+            return (ExpandoObject)expando;
+        }
+
+        private static bool Ignore(PropertyDescriptor property) {
+            return property.Attributes.OfType<Newtonsoft.Json.JsonIgnoreAttribute>().Any();
+        }
+
+        public static dynamic ToResource(this Model model)
+        {
+            var result = model.ToDynamic();
+            result._links = new {
+                self = new {
+                    href = $"/api/models/{model.Code}"
+                }
+            };
+            return result;
+        }
+    }
+}
--- dotnet/Autobarn.Website/Controllers/api/ModelsController.cs
+++ dotnet/Autobarn.Website/Controllers/api/ModelsController.cs
@@ -2,27 +2,57 @@
 using Autobarn.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
+using Autobarn.Website.Models;
 
 namespace Autobarn.Website.Controllers.api {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class ModelsController : ControllerBase {
-		private readonly IAutobarnDatabase db;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController : ControllerBase {
+        private readonly IAutobarnDatabase db;
 
-		public ModelsController(IAutobarnDatabase db) {
-			this.db = db;
-		}
+        public ModelsController(IAutobarnDatabase db) {
+            this.db = db;
+        }
 
-		[HttpGet]
-		public IEnumerable<Model> Get() {
-			return db.ListModels();
-		}
+        [HttpGet]
+        public IActionResult Get() {
+            var result = new {
+                items = db.ListModels().Select(model => model.ToResource())
+            };
+            return Ok(result);
+        }
 
-		[HttpGet("{id}")]
-		public IActionResult Get(string id) {
-			var vehicleModel = db.FindModel(id);
-			if (vehicleModel == default) return NotFound();
-			return Ok(vehicleModel);
-		}
-	}
+        [HttpGet("{id}")]
+        public IActionResult Get(string id) {
+            var vehicleModel = db.FindModel(id);
+            if (vehicleModel == default) return NotFound();
+            var result = vehicleModel.ToResource();
+            result._actions = new
+            {
+                create = new
+                {
+                    method = "POST",
+                    href = $"/api/models/{id}",
+                    name = $"Create a new {vehicleModel.Manufacturer.Name} {vehicleModel.Name}",
+                    type = "application/json"
+                }
+            };
+            return Ok(result);
+        }
+
+        // POST api/models/{modelCode}
+        [HttpPost("{id}")]
+        public IActionResult Post(string id, [FromBody] VehicleDto dto) {
+            var vehicleModel = db.FindModel(id);
+            var vehicle = new Vehicle {
+                Registration = dto.Registration,
+                Color = dto.Color,
+                Year = dto.Year,
+                VehicleModel = vehicleModel
+            };
+            db.CreateVehicle(vehicle);
+            return Created($"/api/vehicles/{dto.Registration}", vehicle);
+        }
+    }
 }
