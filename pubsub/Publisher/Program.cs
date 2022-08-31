4c49d7ca4de61c12a0bf8bfb2d3a35eb06e8ebc3
Dylan Beattoie <dylan@dylanbeattie.net>
2022-08-31 11:59:42 +0200

    Working pub/sub example including error conditon

--- pubsub/Publisher/Program.cs
+++ pubsub/Publisher/Program.cs
@@ -4,9 +4,12 @@ using Messages;
 
 var bus = RabbitHutch.CreateBus("amqps://ivikyigb:EAi4szwzo1_0gQhg-3SS3gPkGgw0JJnn@sweet-pink-deer.rmq4.cloudamqp.com/ivikyigb");
 Console.WriteLine("Press any key to publish a message...");
+int i = 0;
 while (true) {
     Console.ReadKey();
-    var greeting = new Greeting();
+    var greeting = new Greeting() {
+        Name = $"Greeting {i++}"
+    };
     bus.PubSub.Publish(greeting);
     Console.WriteLine($"Published {greeting}");
 } 
--- pubsub/Subscriber/Program.cs
+++ pubsub/Subscriber/Program.cs
@@ -2,9 +2,12 @@
 using Messages;
 
 var bus = RabbitHutch.CreateBus("amqps://ivikyigb:EAi4szwzo1_0gQhg-3SS3gPkGgw0JJnn@sweet-pink-deer.rmq4.cloudamqp.com/ivikyigb");
-bus.PubSub.Subscribe<Greeting>("SUBSCRIPTION_ID", greeting => {
-    Console.WriteLine(greeting);
-});
+bus.PubSub.Subscribe<Greeting>("dylan-beattie", Handle);
 Console.WriteLine("Subscribed to Greeting messsages");
 Console.ReadLine();
 
+
+void Handle(Greeting greeting) {
+    if (greeting.Name.EndsWith("5")) throw new Exception("5 is not allowed!");
+    Console.WriteLine(greeting);
+}
